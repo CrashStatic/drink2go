ec9c9762ab92f63652741868c01d26007915eaeb
CrashStatic <thecrashstatic@gmail.com>
2024-06-22 01:47:05 +0300

    Работает со слайдером

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,58 +1,8 @@
-// const sliderElement = document.querySelector('.slider__list');
-// const prevButtonElement = document.querySelector('.slider__button--prev');
-// const nextButtonElement = document.querySelector('.slider__button--next');
-// const slidesElements = Array.from(sliderElement.querySelectorAll('.slider__item'));
-// const slideCount = slidesElements.length;
-// let slideIndex = 0;
-
-// // Функция для обновления отображения слайдера
-// const updateSlider = () => {
-//   slidesElements.forEach((slide, index) => {
-//     if (index === slideIndex) {
-//       slide.style.display = 'flex';
-//     } else {
-//       slide.style.display = 'none';
-//     }
-//   });
-// };
-
-// // Функция для показа предыдущего слайда
-// const showPreviousSlide = () => {
-//   if (slideIndex !== 0) {
-//     slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-//     updateSlider();
-//   } else {
-//     prevButtonElement.disabled = true;
-//     return slideIndex;
-//   }
-// };
-
-// // Функция для показа следующего слайда
-// const showNextSlide = () => {
-//   if (slideIndex !== slidesElements.length - 1) {
-//     slideIndex = (slideIndex + 1) % slideCount;
-//   } else {
-//     nextButtonElement.disabled = true;
-//   }
-// };
-
-// prevButtonElement.addEventListener('click', showPreviousSlide);
-// nextButtonElement.addEventListener('click', showNextSlide);
-
-// updateSlider();
-
-
 const sliderElement = document.querySelector('.slider__list');
 const slidesElements = Array.from(sliderElement.querySelectorAll('.slider__item'));
 const prevButtonElement = document.querySelector('.slider__button--prev');
 const nextButtonElement = document.querySelector('.slider__button--next');
 let slideIndex = 0;
-
-// const showImage = (index) => {
-//   slidesElements[slideIndex].style.display = 'none';
-//   slidesElements[index].style.display = 'flex';
-//   slideIndex = index;
-// };
 
 const updateSlider = () => {
   slidesElements.forEach((slide, index) => {
@@ -64,28 +14,16 @@ const updateSlider = () => {
   });
 };
 
-// document.querySelector('.slider__button-container').addEventListener('click', (evt) => {
-//   if (evt.target.classList.contains('slider__button--prev') && slideIndex !== 0) {
-//     let index = slideIndex - 1;
-//     if (index < 0) {
-//       index = slidesElements.length - 1;
-//     }
-//     showImage(index);
-//   } else if (evt.target.classList.contains('slider__button--next') && slideIndex !== slidesElements.length - 1) {
-//     let index = slideIndex + 1;
-//     if (index >= slidesElements.length) {
-//       index = 0;
-//     }
-//     showImage(index);
-//   }
-// });
+prevButtonElement.setAttribute('disabled', null);
 
 // Функция для показа предыдущего слайда
 const showPreviousSlide = () => {
   if (slideIndex > 0) {
     slideIndex--;
+    // prevButtonElement.removeAttribute('disabled');
   } else {
     prevButtonElement.ariaDisabled = true;
+    prevButtonElement.setAttribute('disabled', null);
     slideIndex = 0;
   }
   updateSlider(slideIndex);
@@ -95,8 +33,10 @@ const showPreviousSlide = () => {
 const showNextSlide = () => {
   if (slideIndex < slidesElements.length - 1) {
     slideIndex++;
+    prevButtonElement.removeAttribute('disabled');
   } else {
     nextButtonElement.ariaDisabled = true;
+    nextButtonElement.setAttribute('disabled', null);
     slideIndex = slidesElements.length - 1;
   }
   updateSlider(slideIndex);
